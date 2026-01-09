bc7f6aa5dc454a16948ee5c2d6f2beec657a00ce
mahmutessiz <mahmutessiz71@gmail.com>
2026-01-09 10:04:06 +0300

    fix: fix hidration missmatches

--- i18n.config.ts
+++ i18n.config.ts
@@ -2,12 +2,5 @@
 export default defineI18nConfig(() => ({
     legacy: false,
     locale: 'en',
-    messages: {
-        en: {
-            welcome: 'Welcome'
-        },
-        tr: {
-            welcome: 'Hoşgeldiniz'
-        }
-    }
+    messages: {}
 }))
